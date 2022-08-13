42c74e6f54bfc9aad8cd3e70e5d94b468328ac43
NicholasDollick <nidollick@gmail.com>
2022-08-13 01:38:04 -0700

    added file drop section. is currently code behind and not mvvm

--- WPFUserInterface/Views/PDFEditView.xaml.cs
+++ WPFUserInterface/Views/PDFEditView.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFUserInterface.Views
 {
@@ -23,6 +13,35 @@ namespace WPFUserInterface.Views
         public PDFEditView()
         {
             InitializeComponent();
+        }
+
+        // TODO: this isnt MVVM and should be pulled out into a command
+        private void StackPanel_Drop(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                foreach(var file in files)
+                {
+                    MessageBox.Show(Path.GetFileName(file));
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            bool? res = openFile.ShowDialog();
+
+            // TODO: this should filter to only allow for PDFS
+            if(res == true)
+            {
+                string path = openFile.FileName;
+
+                MessageBox.Show(path);
+            }
         }
     }
 }
